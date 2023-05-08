be0f730e293019a021140b119a0b454ace488424
Ioan Zicu <ioan.zicu@nokia.com>
2023-05-08 19:18:31 +0200

    chapter 8.3

---
+++ chapter8/examples/8.3/reverb2/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"time"
+)
+
+func echo(c net.Conn, shout string, delay time.Duration) {
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
+
+func handleConn(c net.Conn) {
+	input := bufio.NewScanner(c)
+	for input.Scan() {
+		go echo(c, input.Text(), 1*time.Second)
+	}
+	c.Close() // ignoring errors
+}
+
+/*
+
+go run reverb1/main.go &
+go run netcat2/main.go
+
+*/
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err) // like connection aborted
+			continue
+		}
+		go handleConn(conn) // handle one connection at a time
+	}
+}
